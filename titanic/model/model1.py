e19c984c240af79304dce2d70fcfd098f3c65d29
Gonzalo Lope Carrasco <64266133+glpcc@users.noreply.github.com>
2023-03-16 14:46:41 +0100

    build model and tester

--- titanic/model/model1.py
+++ titanic/model/model1.py
@@ -2,20 +2,31 @@ import torch
 from torch import nn
 
 class Net(nn.Module):
-    def __init__(self, inputs: int, outputs: int) -> None:
+    def __init__(self, inputs: int, outputs: int,dropout_prob: float) -> None:
         '''
             See the pdf with the diagram to understand the net nomenclature
         '''
         super().__init__()
+        self.num_inputs = inputs
+        self.outputs = outputs
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(inputs, 20),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob),
+            nn.Linear(20, 10),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob),
+            nn.Linear(10, outputs),
+            nn.Sigmoid()
+        )
+        
 
-    def forward(self, inputs):
-        ...
+    def forward(self, inputs: torch.Tensor, batch_num: int) -> torch.Tensor:
+        if inputs.shape != (batch_num,self.num_inputs):
+            raise ValueError(f'Bad Shape if inputs Input Shape:{inputs.shape} != {(batch_num,self.num_inputs)}')
+        out = self.linear_relu_stack(inputs)
+        return out
+
     def clean(self):
         self.state = self.state.detach()*0
         self.prev_output = self.prev_output.detach()*0
-
-    def to(self, *args, **kwargs):
-        self = super().to(*args, **kwargs) 
-        self.state = self.state.to(*args, **kwargs) 
-        self.prev_output = self.prev_output.to(*args, **kwargs) 
-        return self
